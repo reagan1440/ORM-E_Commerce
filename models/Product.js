8d34cc94ce35d3daf4704b81fb998c9eb2989cab
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2024-03-27 06:15:58 -0400

    product.js

--- models/Product.js
+++ models/Product.js
@@ -9,15 +9,32 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: ProductTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: ProductTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
 );
 
 module.exports = Product;
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -27,6 +27,13 @@ ProductTag.init(
       }
     }
   },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
 );
 
 module.exports = ProductTag;
